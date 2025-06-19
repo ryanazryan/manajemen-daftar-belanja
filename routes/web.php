0552bd6e6f454280a37b134ca18a8da14cb9c1c6
Naufal Faiq Azryan <naufalazryan05@gmail.com>
2025-06-19 16:06:14 +0800

    update sedikit

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Mengizinkan akses dari perangkat lain
+        port: 5173, // Port default Vite
+        hmr: {
+            host: 'localhost', // Atau URL Ngrok jika diperlukan
+        },
+    },
 });
